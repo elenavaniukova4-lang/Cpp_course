751b12b56218d3f89a5b1413da5ae6fac4895ae1
elenavaniukova4-lang <elenavaniukova4@yandex.ru>
2025-12-07 00:06:47 +0300

    Update DynamicArray.cc

---
+++ lab_structure/lab2/DynamicArray.cc
@@ -0,0 +1,31 @@
+#ifndef DYNAMICARRAY_H
+#define DYNAMICARRAY_H
+
+#include <stdexcept>
+#include "DynamicArray.cpp"
+
+template<typename T>
+class DynamicArray {
+private:
+    T* data;
+    size_t capacity;
+    size_t length;
+
+public:
+
+    DynamicArray(size_t initialCapacity = 10);
+    ~DynamicArray();
+    DynamicArray(const DynamicArray& other);
+    DynamicArray& operator=(const DynamicArray& other);
+    void push_back(const T& value);
+    void pop_back();
+    T& operator[](size_t index);
+    const T& operator[](size_t index) const;
+    T& back();
+    const T& back() const;
+    size_t size() const;
+    bool empty() const;
+    void resize(size_t newSize);
+};
+
+#endif
